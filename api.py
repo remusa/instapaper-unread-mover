631d66a51828336458a8771b47423f65488709fa
R Sánchez <rene.as.himself@gmail.com>
2018-11-01 20:03:12 -0600

    Added Pipfile

---
+++ api.py
@@ -0,0 +1,27 @@
+import os
+
+# from pyinstapaper.instapaper import Instapaper, Folder
+
+# https://pyinstapaper.readthedocs.io/en/latest/readme.html
+
+""""
+artofmanliness
+austinkleon
+calnewport
+elsyreyes
+fourhourworkweek
+freecodecamp
+jamesaltucher
+lifehacker
+marksdailyapple
+scotthyoung
+youarenotsosmart
+"""
+
+# INSTAPAPER_KEY = os.getenv('INSTAPAPER_KEY', 'API_KEY')  # os.environ
+# INSTAPAPER_SECRET = os.getenv('INSTAPAPER_SECRET', 'API_SECRET')
+# INSTAPAPER_LOGIN = os.getenv('INSTAPAPER_LOGIN', 'MY_EMAIL')
+# INSTAPAPER_PASSWORD = os.getenv('INSTAPAPER_PASSWORD', 'MY_PASSWORD')
+
+# instapaper = Instapaper(INSTAPAPER_KEY, INSTAPAPER_SECRET)
+# instapaper.login(INSTAPAPER_LOGIN, INSTAPAPER_PASSWORD)
--- instapaper.py
+++ instapaper.py
@@ -1,9 +1,13 @@
 import csv
+import json
 import os
+import urllib
+import requests
 
-from pyinstapaper.instapaper import Instapaper, Folder
+from urllib.parse import urlparse
+from urllib.request import urlopen
 
-# https://pyinstapaper.readthedocs.io/en/latest/readme.html
+import oauth2 as oauth
 
 """"
 artofmanliness
@@ -19,19 +23,16 @@ scotthyoung
 youarenotsosmart
 """
 
-INSTAPAPER_KEY = os.getenv('INSTAPAPER_KEY', 'API_KEY')  # os.environ
-INSTAPAPER_SECRET = os.getenv('INSTAPAPER_SECRET', 'API_SECRET')
-INSTAPAPER_LOGIN = os.getenv('INSTAPAPER_LOGIN', 'MY_EMAIL')
-INSTAPAPER_PASSWORD = os.getenv('INSTAPAPER_PASSWORD', 'MY_PASSWORD')
-
-# instapaper = Instapaper(INSTAPAPER_KEY, INSTAPAPER_SECRET)
-# instapaper.login(INSTAPAPER_LOGIN, INSTAPAPER_PASSWORD)
-
 file_original = "instapaper-export.csv"
 filter_term = "fourhourworkweek"
 filter_folder = "Unread"
 file_input = "input.csv"
 file_output = "output-" + filter_folder + "-" + filter_term + ".csv"
+
+INSTAPAPER_KEY = os.getenv('INSTAPAPER_KEY', '0c8a0f75bf004e94bbac21250ad9a78d')  # os.environ.get
+INSTAPAPER_SECRET = os.getenv('INSTAPAPER_SECRET', '3f1fd91ee19f4ffaaa7f7c3069d8c8c1')
+INSTAPAPER_LOGIN = os.getenv('INSTAPAPER_LOGIN', 'rene.msx@gmail.com')
+INSTAPAPER_PASSWORD = os.getenv('INSTAPAPER_PASSWORD', '^bRtcyt%!82R&54^jpXi')
 
 
 def remove_not_unread():
@@ -67,14 +68,31 @@ def filter_unread():
                 print("Error: ", row)
 
 
-def instapaper():
-    print("instapaper")
+def test():
+    consumer_key = INSTAPAPER_KEY
+    consumer_secret = INSTAPAPER_SECRET
+
+    access_token_url = 'https://www.instapaper.com/api/1/oauth/access_token'
+
+    consumer = oauth.Consumer(consumer_key, consumer_secret)
+    client = oauth.Client(consumer)
+
+    params = {"x_auth_username": INSTAPAPER_LOGIN, "x_auth_password": INSTAPAPER_PASSWORD, "x_auth_mode": 'client_auth'}
+
+    client.set_signature_method = oauth.SignatureMethod_HMAC_SHA1()
+    resp, token = client.request(access_token_url, method="POST", body=urllib.parse.urlencode(params))
+    access_token = dict(urllib.parse.parse_qsl(token))
+    print(access_token)
+
+    url = "https://www.instapaper.com/api/1/bookmarks/list"
+    r = requests.get(url)
+    print(json.loads(r.content))
 
 
 def main():
-    remove_not_unread()
-    filter_unread()
-    instapaper()
+    # remove_not_unread()
+    # filter_unread()
+    test()
 
 
 if __name__ == '__main__':
